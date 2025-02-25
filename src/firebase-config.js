2dbab58f86602628ff02d0357d0ab5a8d2395569
matthewlukyanov2 <brucelukyanov@gmail.com>
2025-02-25 17:39:31 +0000

    added content to my firebase config js file given to me from firebase website

---
+++ src/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAVfcLlRqo2q2ln2sfnnNo-BfnhTVN80SI",
+    authDomain: "ai-assistant-fe44a.firebaseapp.com",
+    projectId: "ai-assistant-fe44a",
+    storageBucket: "ai-assistant-fe44a.appspot.com",
+    messagingSenderId: "206896037043",
+    appId: "1:206896037043:web:ab7ee753ce40ded65cc4c6",
+    measurementId: "G-P6L4FXW4RR"
+  };
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);

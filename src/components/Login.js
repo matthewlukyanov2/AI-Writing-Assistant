ca13c1f7a32fe2a4a6e5143f8d0148fc81068e02
matthewlukyanov2 <brucelukyanov@gmail.com>
2025-02-27 18:51:04 +0000

    Set up the login component with firebase and email and password states

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase-config";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+}
+
+export default Login;

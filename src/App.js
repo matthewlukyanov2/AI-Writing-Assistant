8a36fbb1e085ad9969a2af9257a943a4d3d98e7d
matthewlukyanov2 <brucelukyanov@gmail.com>
2025-02-28 22:09:34 +0000

    added logout import to app.js and display logout button

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
+import Logout from "./components/Logout";
 
 function App() {
   return (
     <Router>
+      <div>
+      <h1>AI Writing Assistant</h1>
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
       </Routes>
+      <Logout /> {/* Show logout button */}
+      </div>
     </Router>
   );
 }

9f9120969019b884b38251f7784bf4348fca3527
matthewlukyanov2 <brucelukyanov@gmail.com>
2025-02-28 20:57:49 +0000

    Created logout component with button to sign user out

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -3,4 +3,20 @@ import { signOut } from "firebase/auth";
 import { auth } from "../firebase-config";
 import { useNavigate } from "react-router-dom";
 
+const Logout = () => {
+    const navigate = useNavigate();
+  
+    const handleLogout = async () => {
+      try {
+        await signOut(auth);
+        alert("Logout successful!");
+        navigate("/login"); // Redirect to login page
+      } catch (error) {
+        alert(error.message);
+      }
+    };
+  
+    return <button onClick={handleLogout}>Logout</button>;
+  };
+
 export default Logout;

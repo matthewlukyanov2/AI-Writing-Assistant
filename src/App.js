714ff20e07e22d8e187893297f4ab22105661e60
matthewlukyanov2 <brucelukyanov@gmail.com>
2025-03-02 19:31:26 +0000

    added link import so that logout and signup buttons are displayed on homepage linking to their seperate pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import Logout from "./components/Logout";
@@ -10,12 +10,22 @@ function App() {
     <Router>
       <div>
       <h1>AI Writing Assistant</h1>
+      
+      {/* Buttons for navigation */}
+      <div className="button-container">
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+          <Link to="/signup">
+            <button>Sign Up</button>
+          </Link>
+        </div>
+
       <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/logout" element={<Logout />} />
-      </Routes>
-      <Logout /> {/* Show logout button */}
+      <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
+        </Routes>
       </div>
     </Router>
   );

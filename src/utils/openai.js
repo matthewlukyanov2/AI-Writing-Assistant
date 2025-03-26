00af7ea7b38e5cf155d66a6c7859705c0a009c1e
matthewlukyanov2 <brucelukyanov@gmail.com>
2025-03-26 18:25:52 +0000

    created togetherAI class to implement new ai

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -4,18 +4,18 @@ const API_KEY = "sk-proj-sI3h3t80AwpTojNjfjoNm37qpGZGaPn5_HO0g_CUoKAJhzkwYL33cLR
 const API_URL = "https://api.openai.com/v1/chat/completions";
 
 // Function to add a delay before making an API request
-const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+//const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 export const getAIResponse = async (userInput) => {
-    await delay(5000); // Wait 1 second before making a request
+    //await delay(5000); // Wait 5 seconds before making a request
     try {
-      const response = await axios.post(API_URL, { 
+      const response = await axios.post(API_URL, {
         model: "gpt-3.5-turbo",
         messages: [{ role: "user", content: userInput }]
       }, {
         headers: { Authorization: `Bearer ${API_KEY}`, "Content-Type": "application/json" }
       });
-
+      console.log("Hello Sunshine");
       return response.data.choices[0].message.content;
     } catch (error) {
       if (error.response && error.response.status === 429) {

e5179f4d85781bf001653584652253712c085844
matthewlukyanov2 <brucelukyanov@gmail.com>
2025-04-01 17:11:02 +0100

    added api key

--- src/utils/togetherAI.js
+++ src/utils/togetherAI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = ""; 
+const API_KEY = "8f40a3a594734d80919c26e3f246a018e1c9c0a65bc4fd663e631ed5f25bbdc0"; 
 const API_URL = "https://api.together.xyz/v1/chat/completions";
 
 export const getAIResponse = async (userInput) => {

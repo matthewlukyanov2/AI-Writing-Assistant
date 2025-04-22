d9a7e4d545ae71be81c5635edb486a2e97f31860
matthewlukyanov2 <brucelukyanov@gmail.com>
2025-04-22 15:58:18 +0100

    created Home page to have login and register displayed there

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>Welcome to the AI Assistant</h1>
+      <p>Your friendly tool to improve your writing with AI!</p>
+      <div className="home-buttons">
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/signup">
+          <button>Register</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Home;

ed4c7e7e70e65cf55dd330fe06acc02a92760e00
matthewlukyanov2 <brucelukyanov@gmail.com>
2025-04-22 16:07:39 +0100

    added ui css design to my home page for better look

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link, Navigate  } from "react-router-dom";
+import Home from "./components/Home";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import Logout from "./components/Logout";
@@ -10,23 +11,9 @@ function App() {
   return (
     <Router>
       <div>
-      <h1>AI Writing Assistant</h1>
       
-      {/* Buttons for navigation */}
-      <div className="button-container">
-          <Link to="/login">
-            <button>Login</button>
-          </Link>
-          <Link to="/signup">
-            <button>Sign Up</button>
-          </Link>
-          <Link to="/ai">
-          <button>Use AI Assistant</button>
-          </Link>
-        </div>
-
       <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/" element={<Home />} />
       <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/logout" element={<Logout />} />

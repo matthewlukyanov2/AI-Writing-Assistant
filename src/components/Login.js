9a3d994e7b0409b2ee99cd615825d85579532f94
matthewlukyanov2 <brucelukyanov@gmail.com>
2025-04-23 15:37:11 +0100

    Added css styling to login and created css page

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase-config";
 import { useNavigate } from "react-router-dom";
-import "../styles.css";
+import "./Login.css";
 
 const Login = () => {
     console.log("Login page loaded!");
@@ -22,13 +22,26 @@ const Login = () => {
     };
 
     return (
-       <div>
-        <h2>Login</h2>
-        <form onSubmit={handleLogin}>
-        <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-        <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      <div className="phone-frame login-frame">
+      <div className="notch">
+        <div className="camera-dot"></div>
+      </div>
+      <h1 className="login-title">Login</h1>
+      <form className="login-form" onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
         <button type="submit">Login</button>
-        </form>
+      </form>
        </div>
       );
     };
